81bdefe72df6b67ad61e51bb1dd87f0d54e07244
Eri <erickaponce92@gmail.com>
2024-02-23 12:41:01 -0500

    create Monitor class

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,7 +1,16 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Raton ratonLenovo = new Raton("bluetooth", "Lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoLenovo = new Teclado("Bluetooth", "Lenovo");
+        System.out.println(tecladoLenovo);
+        Monitor monitorLenovo = new Monitor("Lenovo", 25);
+        System.out.println(monitorLenovo);
     }
 }
